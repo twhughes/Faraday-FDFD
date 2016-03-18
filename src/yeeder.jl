a8d62bacae24f019d41a3be4d1db1c3cc62aea77
ShrimCity <tylerwhughes91@gmail.com>
2016-03-17 19:52:52 -0700

    yeeder() function added

--- src/yeeder.jl
+++ src/yeeder.jl
@@ -4,7 +4,7 @@ function yeeder(NGRID,RES,BC,kinc)
         # Calculate yee grid derivative operators
         #
         # Note: for normalized grid, use this function as follows:
-        # [DEX,DEY,DHX,DHY] = yeeder(NGRID,k0*RES,BC,kinc/k0);
+        # (DEX,DEY,DHX,DHY) = yeeder(NGRID,k0*RES,BC,kinc/k0);
         #
         # NGRID = [Nx,Ny]   grid dimensions
         # RES = [dx,dy]     grid resolution of 1x grid
@@ -20,7 +20,48 @@ function yeeder(NGRID,RES,BC,kinc)
         (xbc,ybc) = BC;
         (kx,ky) = kinc;
 
+        N = Nx*Ny;
+        DEX = spzeros(N,N);
+        DEY = spzeros(N,N);
 
+   # Construct DE operators
+        # diagonal terms
+        for i in (1:N)
+            DEX[i,i] = -1;
+            DEY[i,i] = -1
+        end
+
+        # off diagonal terms for DEX
+        for i in (1:N-1)
+            DEX[i,i+1] = 1;
+        end
+
+        # off diagonal terms for DEY
+        for i in (1:N-Nx)
+            DEY[i,i+Nx] = 1;
+        end
+
+        # fix DEX boundary conditions for Dirichlet boundary conditions
+        if (xbc == 0)
+            for i in (Nx:Nx:N-Nx)
+                DEX[i,i+1] = 0;
+            end
+        end
+
+
+
+        DEX = DEX./dx;
+        DEY = DEY./dy;
+
+        DHX = -DEX';
+        DHY = -DEY';
+        return (DEX,DEY,DHX,DHY)
 end
 
-yeeder(NGRID,RES,BC,kinc)
+NGRID = [3 2];
+RES = [0.1 1];
+BC = [0 0];
+kinc = [0 0];
+(DEX,DEY,DHX,DHY) = yeeder(NGRID,RES,BC,kinc);
+full(DEX)
+#full(DEY);
